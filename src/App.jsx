fce6aa1083598ebd83ea4d1e332f3b166fec5237
kaziabulhasib <hasibkazi420@gmail.com>
2025-03-13 22:03:28 +0530

    all files setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ClaimCoupon from "./components/ClaimCoupon";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <ClaimCoupon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ClaimCoupon.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const ClaimCoupon = () => {
+  const [message, setMessage] = useState("");
+  const [coupon, setCoupon] = useState(null);
+
+  const claimCoupon = async () => {
+    try {
+      const response = await fetch(
+        "https://coupon-app-tlkb.onrender.com/api/coupons/claim"
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setCoupon(data.coupon);
+        setMessage(data.message);
+      } else {
+        setMessage(data.message); // Restriction message
+      }
+    } catch (error) {
+      console.error("Error claiming coupon:", error);
+      setMessage("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className='container'>
+      <h1>Claim Your Coupon</h1>
+      <button onClick={claimCoupon}>Claim Coupon</button>
+
+      {message && <p>{message}</p>}
+      {coupon && (
+        <p>
+          Your Coupon: <strong>{coupon}</strong>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ClaimCoupon;
